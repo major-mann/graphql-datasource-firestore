9ffb7d4869432f720f0bb002e919cda5b98b6914
Calvin Mann <calvinmann@gmail.com>
2019-06-03 18:28:20 +0200

    Added the passed through "timestamps" parameter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 module.exports = createGraphqlFirebaseSource;
 
-const LIMIT = 200;
-
 const createGraphqlDatasource = require('@major-mann/graphql-datasource-base');
 const createFirestoreSource = require('@major-mann/datasource-firestore');
 
-async function createGraphqlFirebaseSource({ firestore, definitions, rootTypes, idFieldSelector }) {
+async function createGraphqlFirebaseSource({ firestore, definitions, rootTypes, idFieldSelector, timestamps }) {
     const loadCollection = createFirestoreSource({ firestore });
     const source = await createGraphqlDatasource({
         rootTypes,
+        timestamps,
         definitions,
         idFieldSelector,
         data: loadCollection
